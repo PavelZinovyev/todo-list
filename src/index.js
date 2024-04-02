a9705d933bd6f0be8ebcdaeb5e10634fab4c3ce5
Pavel Zinoviev <pzinoviev@happynumbers.com>
2024-04-02 13:57:10 +0300

    feat: create structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './components/sceens/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
