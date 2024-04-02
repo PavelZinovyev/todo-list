cde6d951c78555cf807ff4af28e2dfbfd9ebcfb1
Pavel Zinoviev <pzinoviev@happynumbers.com>
2024-04-02 19:12:44 +0300

    fix: naming

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './components/sceens/Home/Home';
+import Home from './components/screens/Home/Home';
 import Layout from './components/Layout/Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
